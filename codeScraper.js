5b686d6428172b5f4b8006d1b2c36cc4d2f8d45f
8bajusz <8bajusz@informatik.uni-hamburg.de>
2021-11-26 16:03:43 +0100

    [codeScraper] - add basic body for codeScraper

---
+++ codeScraper.js
@@ -0,0 +1,15 @@
+const openMPDirectives = require('./variables/openMPDirectives');
+
+const codeScraper = {
+    async scrapeCode() {
+        // data which will be passed back to repositoryScraper
+        let data = {};
+
+        // TODO logic
+
+        // return data to repositoryScraper
+        return data;
+    }
+}
+
+module.exports = codeScraper;
