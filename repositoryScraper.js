cdf0943bab6dbef8e21606042627bbd246fae5e8
8bajusz <8bajusz@informatik.uni-hamburg.de>
2021-11-26 11:26:23 +0100

    [repositoryScraper] - get relevant URLs of top-level repository page

--- repositoryScraper.js
+++ repositoryScraper.js
@@ -21,9 +21,11 @@ const repositoryScraper = {
             // TODO logic scraping until relevant code is reached -> call codeScraper() then
 
             let urlTypes = await getInnerURLRepositoriesType();
+            let urlHrefs = await getInnerURLRepositoriesHrefs();
 
-            let relevantURLs = await getRelevantURLs(urlTypes);
-            console.log(relevantURLs)
+            let relevantURLs = await getRelevantURLs(urlTypes, urlHrefs);
+
+            console.log(relevantURLs);
         }
 
         async function checkIfRepositoryIsRelevant() {
@@ -61,40 +63,47 @@ const repositoryScraper = {
         }
 
         async function getInnerURLRepositoriesType() {
-            let innerRepositoryURLsType = await page.$$eval('.repository-content  .js-navigation-container [role="gridcell"] [aria-label]', (arialabels) => {
+            return await page.$$eval('.repository-content  .js-navigation-container [role="gridcell"] [aria-label]', (arialabels) => {
                 let types = [];
                 arialabels.forEach(arialabel => types.push(arialabel.attributes[0].value));
                 return types;
             });
-            return innerRepositoryURLsType;
+        }
+        async function getInnerURLRepositoriesHrefs() {
+            // build list of repository contents
+            return await page.$$eval('.repository-content  .js-navigation-container .Link--primary[href]', (primarylinks) => {
+                let hrefs = [];
+                primarylinks.forEach(primarylink => hrefs.push(primarylink.href));
+                return hrefs;
+            });
         }
 
+        async function getRelevantURLs(urlTypes, urlHrefs) {
+            let filteredHrefs = [];
 
-        async function getRelevantURLs(urlTypes) {
-            var types = urlTypes;
-            console.log(typeof types)
+            let types = urlTypes;
+            let hrefs = urlHrefs;
 
-            // build list of repository contents
-            let relevantInnerRepositoryURLsList = await page.$$eval('.repository-content  .js-navigation-container .Link--primary[href]', (primarylinks) => {
-                let hrefs = [];
-
+            if (types.length === hrefs.length) {
                 // iterate over all links and check their relevance
-                for (let i = 0; i < primarylinks.length; i++) {
+                for (let i = 0; i < types.length; i++) {
                     // directories are directly added to the hrefs list
                     if (types[i] === 'Directory') {
-                        hrefs.push(primarylinks[i].href);
+                        filteredHrefs.push(hrefs[i]);
                     }
                     // files are checked for relevance and added if relevant
                     if (types[i] === 'File') {
-                        // TODO filter primarylinks[i].href file ending for relevance and push into hrefs array if relevant
+                        // iterate over allowedDataTypes array and check if file is relevant
+                        for (let j = 0; j < allowedDataTypes.length; j++) {
+                            if (hrefs[i].indexOf(allowedDataTypes[j]) > -1) {
+                                filteredHrefs.push(hrefs[i]);
+                            }
+                        }
                     }
                 }
-                return hrefs
-            });
-            return relevantInnerRepositoryURLsList;
+            }
+            return filteredHrefs;
         }
-
-
     }
 }
 
