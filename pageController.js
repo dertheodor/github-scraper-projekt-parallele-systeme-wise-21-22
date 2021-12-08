a3b2e5d0bab0034e5ecdc55876e8c5b337938b9b
8bajusz <8bajusz@informatik.uni-hamburg.de>
2021-12-08 14:29:25 +0100

    [pageController] - change way how files are saved, save every topic(category) to an individual JSON file

--- pageController.js
+++ pageController.js
@@ -15,38 +15,45 @@ async function scrapeAll(browserInstance) {
         let scrapedData = {};
 
         // these category URLs will be scraped (respectively for ?l params of fortran, c and  c++)
-        let toBeScrapedURLs = [
+        let toBeScrapedTopicURLs = [
             'https://github.com/topics/science'
         ];
 
         // call the scraper for the URLs
-        for (let i = 0; i < toBeScrapedURLs.length; i++) {
+        for (let i = 0; i < toBeScrapedTopicURLs.length; i++) {
             // regex for getting the topic of the URL
-            let category = toBeScrapedURLs[i].match(/\/topics\/(.*)/)[1];
+            let category = toBeScrapedTopicURLs[i].match(/\/topics\/(.*)/)[1];
 
             // scraper call for all three programming languages
-            const fortranArray = await topicScraper.scrapeTopics(browser, `${toBeScrapedURLs[i]}?l=fortran`);
-            //TODO comment in const cArray = await topicScraper.scrapeTopics(browser, `${toBeScrapedURLs[i]}?l=c`);
-            //TODO comment in const cPlusPlusArray = await topicScraper.scrapeTopics(browser, `${toBeScrapedURLs[i]}?l=c%2B%2B`);
+            const fortranObject = await topicScraper.scrapeTopics(browser, `${toBeScrapedTopicURLs[i]}?l=fortran`);
+            //TODO comment in const cObject = await topicScraper.scrapeTopics(browser, `${toBeScrapedURLs[i]}?l=c`);
+            //TODO comment in const cPlusPlusObject = await topicScraper.scrapeTopics(browser, `${toBeScrapedURLs[i]}?l=c%2B%2B`);
 
-            // TODO change if condition
-            if (typeof fortranArray != "undefined" && typeof cArray != "undefined" && typeof cPlusPlusArray != "undefined") {
-                // concat all arrays
-                scrapedData[`${category}`] = fortranArray.concat(cArray, cPlusPlusArray)
-            }
+            scrapedData['fortran'] = fortranObject;
+            scrapedData['c'] = cObject;
+            scrapedData['c++'] = cPlusPlusObject;
+
+            await saveFile(scrapedData, category);
+            scrapedData = {};
+        }
+
+        /**
+         * Saves each category to a JSON file with the category as its name.
+         * @param data
+         * @param category
+         */
+        function saveFile(data, category) {
+            // write scraped content to JSON file
+            fs.writeFile(`${category}.json`, JSON.stringify(data), 'utf8', function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("The data has been scraped and saved successfully! View it at './data.json'");
+            });
         }
 
         // close browser
         await browser.close();
-
-        // TODO save each topic as own JSON instead of saving all topics in one
-        // write scraped content to JSON file
-        fs.writeFile("data.json", JSON.stringify(scrapedData), 'utf8', function (err) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log("The data has been scraped and saved successfully! View it at './data.json'");
-        });
     } catch (err) {
         console.log("Could not resolve the browser instance => ", err);
     }
