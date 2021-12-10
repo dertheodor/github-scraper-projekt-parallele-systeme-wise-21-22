f31bcecc09c148c87cee3795ce1acfb646e3adba
8bajusz <8bajusz@informatik.uni-hamburg.de>
2021-12-10 12:37:07 +0100

    [codeScraper] - add functionality for scraping code

--- codeScraper.js
+++ codeScraper.js
@@ -1,6 +1,12 @@
 const openMPDirectives = require('./variables/openMPDirectives');
 
 const codeScraper = {
+    /**
+     * Scrapes the code of a single file and returns openMP directive matches.
+     * @param browser
+     * @param url
+     * @returns {Promise<{}>}
+     */
     async scrapeCode(browser, url) {
 
         // data which will be passed back to pageController
@@ -16,15 +22,38 @@ const codeScraper = {
         // Wait for the required DOM to be rendered
         await page.waitForSelector('.Box-body[itemprop="text"]');
 
-        // TODO logic
         let fileCode = await page.$eval('.blob-code-content .highlight.tab-size > tbody', (element) => {
             return element.innerText.replaceAll('\n', '').replaceAll('\t', '');
         })
 
-        // iterate over all directives and search for them in fileCode
-        for (let i = 0; i < openMPDirectives.length; i++) {
-            // TODO
+        if (fileCode) {
+            // fortran code
+            if (fileCode.match(/!\$omp/i)) {
+                await analyzeCode(openMPDirectives.openMPDirectivesFortran)
+            }
+
+            // c/c++ code
+            if (fileCode.match(/#pragma omp/i)) {
+                await analyzeCode(openMPDirectives.openMPDirectivesC)
+            }
         }
+
+        /**
+         * Analyses the code and memorizes the found directives and its occurrences.
+         * @param languageDirectivesArray
+         * @returns {Promise<void>}
+         */
+        async function analyzeCode(languageDirectivesArray) {
+            // iterate over all directives and search for them in fileCode
+            for (let i = 0; i < languageDirectivesArray.length; i++) {
+                if (fileCode.match(languageDirectivesArray[i])) {
+                    data[languageDirectivesArray[i]] = fileCode.match(languageDirectivesArray[i]).length;
+                }
+            }
+        }
+
+        // close tab
+        await page.close();
 
         // return data to repositoryScraper
         return data;
