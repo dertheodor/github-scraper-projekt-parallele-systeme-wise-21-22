cc60bc1a25a70c28c5395468c3681c5427f207b5
8bajusz <8bajusz@informatik.uni-hamburg.de>
2021-12-17 18:51:21 +0100

    [config] - test run and test params

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  * @type {string[]}
  */
 const toBeScrapedTopicURLs = [
-    'https://github.com/topics/science'
+    'https://github.com/topics/chemistry'
 ]
 
 /**
@@ -11,37 +11,40 @@ const toBeScrapedTopicURLs = [
  * @type {number}
  */
 const antiAbuseDetectionTimeout = 1000;
+// TODO maybe change to less than 1000 as with 1000 we are not getting blocked anymore even after hours of scraping
+// TODO perhaps use Math.random() * (1000 - 500) + 500; //random number between 500 and 1000 milliseconds
+// TODO if we keep getting blocked build a method which checks for selectors only available on the error pages and build retry logic
 
 /**
  * Minimum stars count of a repository.
  * @type {number}
  */
-const repositoryStarsCount = 0;
+const repositoryStarsCount = 20;
 
 /**
  * Minimum forks count of a repository.
  * @type {number}
  */
-const repositoryForksCount = 0;
+const repositoryForksCount = 1;
 
 /**
  * Minimum latest commit date of a repository.
  * e.g.: '2021-10-14T13:27:45Z'
  * @type {string}
  */
-const repositoryLatestCommitDate = "0";
+const repositoryLatestCommitDate = "2021-01-01T00:00:00Z";
 
 /**
  * Minimum commits count of a repository.
  * @type {number}
  */
-const repositoryCommitsCount = 0;
+const repositoryCommitsCount = 10;
 
 /**
  * Minimum contributors count of a repository.
  * @type {number}
  */
-const repositoryContributorsCount = 1;
+const repositoryContributorsCount = 2;
 
 // TODO add count of files checked, count together on different levels
 
