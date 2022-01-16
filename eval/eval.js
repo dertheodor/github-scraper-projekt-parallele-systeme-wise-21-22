3828d8045ac5514bed83443e26328b81bbcf3dfa
8bajusz <8bajusz@informatik.uni-hamburg.de>
2022-01-16 15:16:08 +0100

    add functionality for evaluating json files

---
+++ eval/eval.js
@@ -0,0 +1,85 @@
+/*
+// 1. Zusammenrechnen für jede Topic pro Sprache(Fortran, C, C++):
+ // Beispiel für C mit einem Objekt im Array wo alles drin steht (keine Ahnung was besser ist)
+{
+  "c": [
+    {
+      "#pragma omp parallel": X,
+      "#pragma omp for ": X,
+      "#pragma omp parallel for": X,
+      "quantityOfFiles": X,
+      "quantityOfFilesWithOpenMP": X
+      "quantityOfRepositories": X,
+      "quantityOfRelevantRepositories": X,
+      "quantityOfRelevantRepositoriesWithOpenMP": X
+    }
+  ]
+}
+
+//2. Zusammenrechnen für jede Wissenschaft(besteht aus mehreren Topics) pro Sprache
+// Dateiname z.B.: chemistryLanguageAggregation.json
+// Gleicher Inhalt wie oben, auch aufgeteilt auf die drei Sprachen
+
+
+//3. Zusammenrechnen für jede Wissenschaft
+// Dateiname z.B.: chemistryAggregation.json
+// Gleicher Inhalt wie oben, nun aber nicht mehr aufgeteilt auf Sprachen, sondern über alle Sprachen hinweg zusammengerechnet
+ */
+
+const fs = require('fs')
+
+/**
+ *
+ * @param science
+ * @param jsonFileName
+ */
+function evaluateTopic(science, jsonFileName) {
+    var evaluatedTopic = {};
+    var jsonContent = require(`../results/base-results/${science}/${jsonFileName}.json`);
+
+    var languages = ["fortran", "c", "c++"]
+    var quantityOfFiles = 0;
+    var quantityOfFilesWithOpenMP = 0;
+    var quantityOfRepositories = 0;
+    var quantityOfRelevantRepositories = 0;
+    var quantityOfRelevantRepositoriesWithOpenMP = 0;
+
+    for (let i = 0; i < languages.length; i++) {
+        for (let j = 0; j < jsonContent[languages[i]].length; j++) {
+            // quantityOfFiles and quantityOfFilesWithOpenMP
+            if (jsonContent[languages[i]][j]["quantityOfFilesWithOpenMP"]) {
+                quantityOfFiles += jsonContent[languages[i]][j]["quantityOfFiles"];
+                quantityOfFilesWithOpenMP += jsonContent[languages[i]][j]["quantityOfFilesWithOpenMP"];
+            }
+
+            // repos
+            if (jsonContent[languages[i]][j]["quantityOfRepositories"]) {
+                quantityOfRepositories += jsonContent[languages[i]][j]["quantityOfRepositories"];
+                quantityOfRelevantRepositories += jsonContent[languages[i]][j]["quantityOfRelevantRepositories"];
+                quantityOfRelevantRepositoriesWithOpenMP += jsonContent[languages[i]][j]["quantityOfRelevantRepositoriesWithOpenMP"];
+            }
+        }
+    }
+
+    evaluatedTopic['quantityOfFiles'] = quantityOfFiles;
+    evaluatedTopic['quantityOfFilesWithOpenMP'] = quantityOfFilesWithOpenMP;
+    evaluatedTopic['quantityOfRepositories'] = quantityOfRepositories;
+    evaluatedTopic['quantityOfRelevantRepositories'] = quantityOfRelevantRepositories;
+    evaluatedTopic['quantityOfRelevantRepositoriesWithOpenMP'] = quantityOfRelevantRepositoriesWithOpenMP;
+
+
+
+
+
+
+    // write evaluated contents to new json
+    fs.writeFile(`./results/evaluated-topic-results/${science}/evaluated${jsonFileName}.json`, JSON.stringify(evaluatedTopic), 'utf8', function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(`The data has been successfully evaluated.`);
+    });
+}
+
+evaluateTopic('physics', 'physics');
+
